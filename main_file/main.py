894fb1abcf8c28caf3de1dbf82c26e9fc973f9e6
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-21 20:09:58 -0300

    Moving modules

--- main_file/main.py
+++ main_file/main.py
@@ -1,11 +1,36 @@
-import passgen
+from passgen import password_gen
+from importxt import importxt, show_imported_passwords, existing_passwords, imported_filename
+from exportxt import exporttxt, is_valid_path
+from menu import show_menu, import_menu, export_menu, menu_passgen
+from passgen import password_gen
+import os
 
-try:
-    existing_passwords = set(passgen.passimporttxt(
-        filename='C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\Py\Random-Password-Generator\\passwords\\Minhas Senhas.txt'))
-except TypeError:
-    ...
+generated_passwords = None
 
-generated_passwords = passgen.password_gen(length=8, quantity=5, repeat=False)
-passgen.passexporttxt(generated_passwords,
-                      filename='C:\\Users\\jonat\\OneDrive\\Área de Trabalho\\Py\Random-Password-Generator\\passwords\\Minhas senhas.txt')
+while True:
+
+    show_imported_passwords(existing_passwords, imported_filename)
+
+    option = show_menu()
+
+    match option:
+
+        case '1':
+
+            os.system('cls')
+            existing_passwords, imported_filename = import_menu(
+                func_import=importxt, func_valid_path=is_valid_path)
+
+        case '2':
+
+            os.system('cls')
+            export_menu(func_exportxt=exporttxt,
+                        func_valid_path=is_valid_path, passwords=generated_passwords)
+
+        case '3':
+
+            generated_passwords = menu_passgen(password_gen)
+
+        case '4':
+
+            break
